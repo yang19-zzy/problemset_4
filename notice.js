f78bf088e6209e37134d4be48519df95277589f2
yang19-zzy <zzyang@umich.edu>
2021-02-19 20:26:33 -0500

    first commit

---
+++ notice.js
@@ -0,0 +1,87 @@
+setInterval(function() {
+  const myColor = document.getElementById("notice");
+  let className = myColor.getAttribute("class");
+  if (className === "color1") {
+    myColor.setAttribute("class","color2");
+  } else {
+    myColor.setAttribute("class","color1");
+  }
+}, 2000);
+
+
+
+
+let numItems = document.getElementById("num-items");
+console.log("numItems",numItems.textContent);
+const switch_ = document.getElementById("switch");
+const ringfit = document.getElementById("fit-ring");
+
+// ringfit
+let ringfitNum = document.getElementById("ringfit-num");
+const addbtn_ringfit = document.getElementById("ringfit-add");
+const minusbtn_ringfit = document.getElementById("ringfit-minus");
+addbtn_ringfit.addEventListener("click",()=>{
+  let i = parseInt(ringfitNum.textContent);
+  ringfitNum.textContent = i+1;
+  console.log("ringfitNum",ringfitNum.textContent);
+  numItems.innerText = parseInt(ringfitNum.textContent) + parseInt(switchNum.textContent);
+  cartLists[ringfit] = i;
+});
+minusbtn_ringfit.addEventListener("click",()=>{
+  let i = parseInt(ringfitNum.textContent);
+  if (i > 1) {
+    ringfitNum.textContent = i-1;
+  } else {
+    ringfitNum.textContent = 0;
+  };
+  numItems.innerText = parseInt(ringfitNum.textContent) + parseInt(switchNum.textContent);
+  cartLists[ringfit] = i;
+});
+
+// switch
+let switchNum = document.getElementById("switch-num");
+
+const addbtn_switch= document.getElementById("switch-add");
+const minusbtn_switch = document.getElementById("switch-minus");
+addbtn_switch.addEventListener("click",()=>{
+  let i = parseInt(switchNum.textContent);
+  switchNum.textContent = i+1;
+  console.log("switchNum",switchNum.textContent);
+  numItems.innerText = parseInt(ringfitNum.textContent) + parseInt(switchNum.textContent);
+  cartLists[ringfit] = i;
+});
+minusbtn_switch.addEventListener("click",()=>{
+  let i = parseInt(switchNum.textContent);
+  if (i > 1) {
+    switchNum.textContent = i-1;
+  } else {
+    switchNum.textContent = 0;
+  };
+  numItems.innerText = parseInt(ringfitNum.textContent) + parseInt(switchNum.textContent);
+  cartLists[ringfit] = i;
+});
+
+// console.log('carlist',cartLists);
+// console.log(numItems.textContent);
+// numItems.innerText = parseInt(ringfitNum.textContent) + parseInt(switchNum.textContent);
+// for (i=0;i<buttons.length;i++) {
+//   buttons[i].addEventListener("click", ()=>{
+//     count++;
+//     showNumItems(count);
+//   });
+// };
+
+// close notice
+let myNotice = document.getElementById("notice");
+function closeNotice() {
+  console.log('notice closed');
+}
+
+myNotice.addEventListener("click", (event)=>{
+  console.log('notice clicked')
+  console.log("target id:", event.target.id);
+  if (event.target.id === "close"){
+    console.log(myNotice);
+    myNotice.style.display = "none";
+  }
+});
